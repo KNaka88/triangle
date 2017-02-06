0be846fb3da8a0ad7b23951b0565c31971f3d0dc
Koji Nakagawa and Philip Putnam <koji@>
2017-02-06 15:07:58 -0800

    add home button to triangle page

--- app/app.php
+++ app/app.php
@@ -3,14 +3,13 @@
     require_once __DIR__."/../src/Triangle.php";
 
     $app = new Silex\Application();
-
     $app->get("/", function() {
         return "
         <!DOCTYPE html>
         <html>
           <head>
             <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-            <link rel='stylesheet' href='../css/style.css' type='text/css'>
+            <!-- <link rel='stylesheet' href='../css/style.css' type='text/css'> -->
             <title>Triangle Check</title>
           </head>
 
@@ -42,13 +41,27 @@
     $app->get("/triangle", function(){
         $userTriangle = new Triangle($_GET["length_one"], $_GET["length_two"], $_GET["length_three"]);
         $userTriangleType = $userTriangle->triangleType();
+
         return "
-        <h1>Results are here</h1>
-        <p>Length One:". $userTriangle->getLengthOne(). "</p>
-        <p>Length Two:". $userTriangle->getLengthTwo(). "</p>
-        <p>Length Three:". $userTriangle->getLengthThree()."</p>
-        <p>Your triangle type is $userTriangleType.</p>
-        ";
+
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            <!-- <link rel='stylesheet' href='../css/style.css' type='text/css'> -->
+            <title>Triangle Check</title>
+          </head>
+          <body>
+            <h1>Results are here</h1>
+            <p>Length One:". $userTriangle->getLengthOne(). "</p>
+            <p>Length Two:". $userTriangle->getLengthTwo(). "</p>
+            <p>Length Three:". $userTriangle->getLengthThree()."</p>
+            <p>Your triangle type is $userTriangleType.</p>
+            <a href='/'>
+              <button class='btn btn-success'>Back to Home</button>
+            </a>
+          </body>
+          ";
     });
 
 
